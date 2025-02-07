1134e5847488904c1cf4866946495da0f6b168a7
Amyansu <amyansutripathy09@gmail.com>
2025-02-07 11:48:03 +0530

    added checkbox in the query params of checkbox api

--- src/components/ui/TodoCard.tsx
+++ src/components/ui/TodoCard.tsx
@@ -15,7 +15,7 @@ function TodoCard({ item }: { item: todos }) {
   }
 
   async function CheckBox(id: number) {
-    const res = await fetch(`http://localhost:3001/checkbox/${id}`, {
+    const res = await fetch(`http://localhost:3001/checkbox/${id}?checkbox=${!item.checkbox}`, {
       method: "PUT",
     });
     const data = await res.json();
